6b75b10771f6daa7006972fa8569439e1a5c1b6c
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2019-09-20 22:43:06 +0200

    Transformaties

---
+++ 04-Variabelen/Transformaties.py
@@ -0,0 +1,13 @@
+#invoer
+a = int(input('geef a: '))
+b = int(input('geef b: '))
+
+#berekening
+a2 = 3 + a
+b2 = 4 + b
+g = '2(x - ' + str(a2) + ')^2 + ' + str(b2)
+
+#uivoer
+print('f(x) = 2(x - 3)^2 + 4')
+print('g(x) =',g)
+
