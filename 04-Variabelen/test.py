090561dbbd49301d200905f18102e9fc7cb64ac8
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2019-09-20 11:25:13 +0200

    patroontje

---
+++ 04-Variabelen/test.py
@@ -0,0 +1,18 @@
+# wisselen van waarde
+# x, y = 2, 'hallo'
+# x, y = y, x
+# print(x, y)
+
+# programma die discriminant berekent
+# invoer
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+
+print(a, b, c)
+# berekening
+discriminant = b**2 - (4 * a * c)
+
+# uitvoer
+print(discriminant)
+#------------------------------------
