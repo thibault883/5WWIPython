8e1cc936be0f6fbaaee84f49b37b3112a9eb0237
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2019-09-20 11:36:00 +0200

    patroontje

---
+++ 04-Variabelen/oppervlakte cirkel.py
@@ -0,0 +1,9 @@
+# invoer
+r = float(input('geef de straal van de cirkel: '))
+pi = 3.14159
+
+# berekening
+o = r**2 * pi
+
+# uitvoer
+print('De oppervlakte van een cirkel met straal ' + str(r) + ' is ' + str(o))
