ffab38370008b2cc9c3851c072bba7e8f153f349
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2019-09-27 10:39:58 +0200

    secondje

---
+++ toets/secondje.py
@@ -0,0 +1,15 @@
+#invoer
+sec = int(input("geef aantal seconden: "))
+
+#bereking
+dagen = sec // 86400
+sec %= 86400
+
+uren = sec // 3600
+sec %= 3600
+
+min = sec // 60
+sec %= 60
+
+#uitvoer
+print(str(dagen) + "d " + str(uren) + ":" + str(min) + ":" + str(sec))
