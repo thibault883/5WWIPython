5037955c24430d3d35ed210eda9d17d99d409f04
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2019-10-04 21:17:34 +0200

    de diatomist

--- 05-eenvoudige functies/de diatomist.py
+++ 05-eenvoudige functies/de diatomist.py
@@ -2,8 +2,12 @@
 straal_klein = float(input("geef de straal van de kleine cirkel: "))
 straal_groot = float(input("geef de straal van de grote cirkel: "))
 
+from math import pi, floor
 #berekening
-n = int(0.83*(pow(straal_groot, 2) / pow(straal_klein, 2)) - 1.9)
+
+aantal_cirkel = floor(0.83*(pow(straal_groot, 2) / pow(straal_klein, 2)) - 1.9)
+opp_bedekt = (aantal_cirkel * pow(straal_klein, 2) * pi) / (pow(straal_groot, 2) * pi) * 100
 
 #uitvoer
-print(n)
+uitvoer = "{:d} kleine cirkels bedekken {:.2f}% van de grote cirkel"
+print(uitvoer.format(aantal_cirkel, opp_bedekt))
