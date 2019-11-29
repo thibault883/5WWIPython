185044698fe046022ff17879a6e35fe4dcc95b6e
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2019-11-29 11:35:26 +0100

    atoommodel van BOHR

--- 07b-while.lus/rad van fortuin.py
+++ 07b-while.lus/rad van fortuin.py
@@ -1,5 +1,12 @@
 woord = input('geef een woord: ')
 geld = int(input('geef geld: '))
+letter = input('geef de letter: ')
+totaal = 0
+vorige_letter = ''
+while letter in woord and vorige_letter != letter :
+    vorige_letter = letter
+    totaal += geld
+    letter = input('geef de letter: ')
 
-while
 
+print(totaal)
