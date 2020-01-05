12e96577c11a2b58a4cfe75fea5b70491fb8ed6b
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-01-05 15:10:18 +0100

    atoommodel van BOHR

--- 07b-while.lus/Duitse Tanks.py
+++ 07b-while.lus/Duitse Tanks.py
@@ -2,7 +2,9 @@ reeks = int(input("geef het nummer: "))
 aantal,hoogste = 0,0
 
 while reeks > 0:
+    hoogste = max(reeks, hoogste)
     reeks = int(input("geef het nummer: "))
     aantal += 1
-    hoogste = max(reeks, hoogste)
 
+tanks = round((((aantal + 1) * hoogste) / (aantal)) - 1)
+print("Het aantal geproduceerde tanks wordt geschat op " + str(tanks) + ".")
