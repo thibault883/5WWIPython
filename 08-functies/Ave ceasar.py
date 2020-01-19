d47db1d4ae9a1dcd2deae8ea0b7d868ff985ea61
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-01-19 19:28:21 +0100

    atoommodel van BOHR

--- 08-functies/Ave ceasar.py
+++ 08-functies/Ave ceasar.py
@@ -5,10 +5,14 @@ def roteer_letter(t, aantal):
     if is_letter(t) == 'True':
         letter = ord(t) + aantal
         if ord('a') <= letter <= ord('z') or ord('A') <= letter <= ord('Z'):
-            chr(letter)
+            letter = chr(letter)
         else:
             if ord('a') <= ord(t) <= ord('z'):
-                letter = letter - ord(t) + 96
-            else ord('A') <= ord(t) <= ord('Z'):
-                letter = letter - ord(t) + 64
+                letter = chr(letter - ord(t) + 96)
+            elif ord('A') <= ord(t) <= ord('Z'):
+                letter = chr(letter - ord(t) + 64)
+
+    return letter
+
+print(roteer_letter('e', 4))
 
