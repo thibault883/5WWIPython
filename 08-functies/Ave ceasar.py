e5e55ad1298bb1df55bccb9ebf9012c3d3915e80
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-01-20 17:50:27 +0100

    atoommodel van BOHR

--- 08-functies/Ave ceasar.py
+++ 08-functies/Ave ceasar.py
@@ -2,17 +2,24 @@ def is_letter(t):
     return ord('a') <= ord(t) <= ord('z') or ord('A') <= ord(t) <= ord('Z')
 
 def roteer_letter(t, aantal):
-    if is_letter(t) == 'True':
+    if str(is_letter(t)) == 'True':
         letter = ord(t) + aantal
-        if ord('a') <= letter <= ord('z') or ord('A') <= letter <= ord('Z'):
-            letter = chr(letter)
-        else:
-            if ord('a') <= ord(t) <= ord('z'):
-                letter = chr(letter - ord(t) + 96)
-            elif ord('A') <= ord(t) <= ord('Z'):
-                letter = chr(letter - ord(t) + 64)
+        if ord('a') <= ord(t) <= ord('z'):
+            if not ord('a') <= letter <= ord('z'):
+                letter -= 26
+        elif ord('A') <= ord(t) <= ord('Z'):
+            if not ord('A') <= letter <= ord('Z'):
+                letter -=  26
+    else:
+        letter = ord(t)
 
-    return letter
+    return chr(letter)
 
-print(roteer_letter('e', 4))
+def versleutel(zin, aantal):
+    uitkomst = ''
+    for t in zin:
+        uitkomst += roteer_letter(t, aantal)
+
+    return uitkomst
+
 
--- 08-functies/test_2.py
+++ 08-functies/test_2.py
@@ -1,11 +1,18 @@
 from random import randint
 
-def gooi_muntstuk():
-    muntstuk = 'munt'
-    if not randint(0,2):
-        muntstuk = 'kop'
-    return muntstuk
+#def gooi_muntstuk():
+    #muntstuk = 'munt'
+    #if not randint(0,2):
+     #   muntstuk = 'kop'
+    #return muntstuk
 
-for i in range(10):
-    print(gooi_muntstuk())
+#for i in range(10):
+#    print(gooi_muntstuk())
 #print(rg, muntstuk)
+
+zin = input('geef een zin: ')
+uitkomst = ''
+for t in zin:
+    uitkomst = uitkomst + t
+print(uitkomst)
+
