ee76b13a0cd4b9a5d5e43a66978fb55991cf7db7
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-02-03 22:01:19 +0100

    atoommodel van BOHR

--- 08-functies/6174.py
+++ 08-functies/6174.py
@@ -3,53 +3,47 @@ def splits(getal):
     for i in str(getal):
         stap += 1
         if stap == 1:
-            getal_1 = i
+            getal_1 = int(i)
         elif stap == 2:
-            getal_2 = i
+            getal_2 = int(i)
         elif stap == 3:
-            getal_3 = i
+            getal_3 = int(i)
         else:
-            getal_4 = i
+            getal_4 = int(i)
 
     return getal_1, getal_2, getal_3, getal_4
 
-def oplopende_cijfers(getal_1, getal_2, getal_3, getal_4):
-    hoogste = max(getal_1, getal_2, getal_3, getal_4)
-    laagste = min(getal_1, getal_2, getal_3, getal_4)
-    mid_1 = int(max(min(getal_1, getal_2), min(getal_1, getal_3), min(getal_3, getal_2)))
-    mid_2 = int(getal_3) + int(getal_2) + int(getal_1) + int(getal_4) - int(hoogste) - int(laagste) - int(mid_1)
+def oplopende_cijfers(a, b, c, d):
+    hoogste = max(a, b, c, d)
+    laagste = min(a, b, c, d)
+    mid_1 = int(max(min(a, b), min(a, c), min(b, c)))
+    mid_2 = a + b + c + d - hoogste - laagste - mid_1
     mid_laag = min(mid_1, mid_2)
     mid_hoog = max(mid_1, mid_2)
     return laagste, mid_laag, mid_hoog, hoogste
 
-def op_af_getallen(laagste, mid_laag, mid_hoog, hoogste):
-    cijfer_1 = str(laagste) + str(mid_laag) + str(mid_hoog) + str(hoogste)
-    cijfer_2 = str(hoogste) + str(mid_hoog) + str(mid_laag) + str(laagste)
+def op_af_getallen(a, b, c, d):
+    cijfer_1 = str(a) + str(b) + str(c) + str(d)
+    cijfer_2 = str(d) + str(c) + str(b) + str(a)
 
     return cijfer_1, cijfer_2
 
-def verschil(cijfer_1, cijfer_2):
-    verschil = int(cijfer_1) - int(cijfer_2)
+def verschil(a, b):
+    verschil = int(a) - int(b)
 
-    return verschil
+    return str(verschil)
 
 def kaprekar(getal):
-    while uitvoer != 6174:
+    uitkomst = ''
+    while getal != 6174:
         getal_1, getal_2, getal_3, getal_4 = splits(getal)
         laagste, mid_laag, mid_hoog, hoogste = oplopende_cijfers(getal_1, getal_2, getal_3, getal_4)
         cijfer_1, cijfer_2 = op_af_getallen(laagste, mid_laag, mid_hoog, hoogste)
-        uitvoer = verschil(cijfer_2, cijfer_1)
-        return uitvoer
-        getal = uitvoer
+        getal = int(verschil(cijfer_2, cijfer_1))
+        uitkomst += '{} - {} = {}\n'.format(cijfer_2, cijfer_1, getal)
+
+    return uitkomst.strip()
 
 
 
-def leuk(getal):
-    uitvoer = ''
-    while getal != 5:
-        uitvoer += str(getal) + "\n"
-        getal += 1
-    return uitvoer
 
-
-print(leuk(2))
