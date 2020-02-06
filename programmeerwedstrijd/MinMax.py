f07526eff680b0b67860a9d6456a69684485c6fe
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-02-06 18:34:18 +0100

    ha

---
+++ programmeerwedstrijd/MinMax.py
@@ -0,0 +1,13 @@
+lijsten = int(input('hoeveel keer: '))
+uitkomst = ''
+for i in range(lijsten):
+    hoogste, laagste= 0, 1000
+    aantal = int(input('hoeveel in lijst: '))
+    for k in range(aantal):
+        cijfer = int(input('geef een cijfer: '))
+        hoogste = max(cijfer, hoogste)
+        laagste = min(cijfer, laagste)
+    uitkomst += '{} {} {}\n'.format(i + 1, laagste, hoogste)
+
+print(uitkomst[:-1])
+
