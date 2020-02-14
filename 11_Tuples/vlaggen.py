7c910058797c23833349cb0cf00487c4e55fa802
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-02-14 11:24:09 +0100

    ha

---
+++ 11_Tuples/vlaggen.py
@@ -0,0 +1,11 @@
+def vlag(soort, kleur):
+    uitvoer = ''
+    for i in range(len(kleur)):
+        if soort == 'verticaal':
+            uitvoer += kleur[i] + ' | '
+        else:
+            uitvoer += kleur[i] + '\n' + '-\n'
+
+    return uitvoer[:-3]
+
+print(vlag('verticaal',('zwart', 'geel', 'rood')))
