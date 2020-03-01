baf6472eeff5e329f240e8c45780b076decd975d
thibault ghekiere <thibault.ghekiere@sgsintpaulus.eu>
2020-03-01 12:32:10 +0100

    ha

--- krokusvertier/Vergiftigde wijn.py
+++ krokusvertier/Vergiftigde wijn.py
@@ -1,7 +1,7 @@
 aantal = int(input('geef hoeveel dood: '))
-fles, nummer = 0, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+fles = 0
 for i in range(aantal):
     gevangen = input('geef de gevangene: ')
-    fles += pow(2, nummer[ord(gevangen) - 65])
+    fles += pow(2, ord(gevangen) - 65)
 
 print('Fles #{} is vergiftigd.'.format(fles))
